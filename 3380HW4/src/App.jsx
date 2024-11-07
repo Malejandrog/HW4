e250beb803d2273f6cf7e1b600c531f9a60b119f
Alejandro Martinez <Malejandrog2000@gmail.com>
2024-11-06 20:07:01 -0600

    List correctly add items, new clear order button

--- 3380HW4/src/App.jsx
+++ 3380HW4/src/App.jsx
@@ -1,146 +1,89 @@
-import { useState } from 'react'
-import './App.css'
-import { Flex, FormControl, FormLabel } from '@chakra-ui/react'
+import { useState } from 'react';
+import './App.css';
+import { Flex, FormControl, FormLabel } from '@chakra-ui/react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [orderItems, setOrderItems] = useState([]);
+
+  const menuItems = [
+    { name: 'The Databurger', price: 8.99, img: 'Burger1.png' },
+    { name: 'The Double Smash Patty', price: 9.99, img: 'Burger2.png' },
+    { name: 'The Chicken Sandwich', price: 7.99, img: 'ChickenSandwich.png' },
+    { name: 'Chicken Tenders', price: 6.99, img: 'ChickenTender.png' },
+    { name: 'Databurger Meal', price: 12.99, img: 'b1Meal.png' },
+    { name: 'Double Patty Meal', price: 13.99, img: 'b2Meal.png' },
+    { name: 'Chicken Sandwich Meal', price: 11.99, img: 'CSMeal.png' },
+    { name: 'Chicken Tender Meal', price: 10.99, img: 'CTMeal.png' },
+    { name: 'Chicken Salad', price: 6.99, img: 'Salad.png' },
+    { name: 'Fountain Drink', price: 1.99, img: 'Drink.png' },
+    { name: 'French Fries', price: 2.99, img: 'Fries.png' },
+    { name: 'Ice Cream', price: 3.99, img: 'IceCream.png' },
+  ];
+
+  const addItemToOrder = (item) => {
+    setOrderItems((prevItems) => [...prevItems, item]);
+    setCount((prevCount) => prevCount + item.price);
+  };
+
+  const clearOrder = () => {
+    setOrderItems([]);
+    setCount(0);
+  };
 
   return (
     <>
       <div className='topBar'>
         <h1>Databurger</h1>
-        <img src="public/Databurger.png" className="Logo"></img>
+        <img src="public/Databurger.png" className="Logo" alt="Logo" />
       </div>
-      
 
-  <div className="mainContainer">
-    <div class="menuArea">
-
-      <button onClick={() => setCount((count) => count + 8.99)} class="menuItem">
-        <div>
-          <img src="public/Burger1.png" className="Burger1" alt="Burger1" />
-          <p>The Databurger $8.99</p>
+      <div className="mainContainer">
+        <div className="menuArea">
+          {menuItems.map((item, index) => (
+            <button key={index} onClick={() => addItemToOrder(item)} className="menuItem">
+              <div>
+                <img src={`public/${item.img}`} className={item.img.replace('.png', '')} alt={item.name} />
+                <p>{item.name} ${item.price.toFixed(2)}</p>
+              </div>
+            </button>
+          ))}
         </div>
-      </button>
-      
-      <button onClick={() => setCount((count) => count + 9.99)} class="menuItem">
-        <div>
-          <img src="public/Burger2.png" className="Burger2" alt="Burger2" />
-          <p>The Double Smash Patty $9.99</p>
+
+        <div className='paymentInfo'>
+          <p className='orderInfo'>Order Info:</p>
+          <ul>
+            {orderItems.map((item, index) => (
+              <li key={index}>{item.name} - ${item.price.toFixed(2)}</li>
+            ))}
+          </ul>
+
+          <button onClick={clearOrder} className="clearButton">Clear Order</button>
+
+          <label htmlFor="CCInfo">Credit Card Number:</label>
+          <input type="text" id="CCInfo" />
+
+          <label htmlFor="CCV">CCV:</label>
+          <input type="text" id="CCV" />
+
+          <label htmlFor="CCDate">Exp. Date:</label>
+          <input type="text" id="CCDate" />
+
+          <label htmlFor="Address">Address:</label>
+          <input type="text" id="Address" />
+
+          <label htmlFor="Tip">Tip:</label>
+          <input type="text" id="Tip" />
+
+          <p><b>Total Price: ${(count*1.0825).toFixed(2)}</b></p>
+
+          <input type="button" className="orderButton" value="Place Order" />
         </div>
-      </button>
 
-      <button onClick={() => setCount((count) => count + 7.99)} class="menuItem">
-        <div>
-          <img src="public/ChickenSandwich.png" className="ChickenSandwich" alt="ChickenSandwich" />
-          <p>The Chicken Sandwich $7.99</p>
-        </div>
-      </button>
-
-      <button onClick={() => setCount((count) => count + 6.99)} class="menuItem">
-        <div>
-          <img src="public/ChickenTender.png" className="ChickenTender" alt="ChickenTender" />
-          <p>Chicken Tenders $6.99</p>
-        </div>
-      </button>
-
-      <button onClick={() => setCount((count) => count + 12.99)} class="menuItem">
-        <div>
-          <img src="public/b1Meal.png" className="b1Meal" alt="b1Meal" />
-          <p>Databurger Meal $12.99</p>
-        </div>
-      </button>
-
-      <button onClick={() => setCount((count) => count + 13.99)} class="menuItem">
-        <div>
-          <img src="public/b2Meal.png" className="b2Meal" alt="b2Meal" />
-          <p>Double Patty Meal $13.99</p>
-        </div>
-      </button>
-
-      <button onClick={() => setCount((count) => count + 11.99)} class="menuItem">
-        <div>
-          <img src="public/CSMeal.png" className="CSMeal" alt="CSMeal" />
-          <p>Chicken Sandwhich Meal $11.99</p>
-        </div>
-      </button>
-
-      <button onClick={() => setCount((count) => count + 10.99)} class="menuItem">
-        <div>
-          <img src="public/CTMeal.png" className="CTMeal" alt="CTMeal" />
-          <p>Chicken Tender Meal $10.99</p>
-        </div>
-      </button>
-
-      <button onClick={() => setCount((count) => count + 6.99)} class="menuItem">
-        <div>
-          <img src="public/Salad.png" className="Salad" alt="Salad" />
-          <p>Chicken Salad $6.99</p>
-        </div>
-      </button>
-
-      <button onClick={() => setCount((count) => count + 1.99)} class="menuItem">
-        <div>
-          <img src="public/Drink.png" className="Drink" alt="Drink" />
-          <p>Fountain Drink $1.99</p>
-        </div>
-      </button>
-
-      <button onClick={() => setCount((count) => count + 2.99)} class="menuItem">
-        <div>
-          <img src="public/Fries.png" className="Fries" alt="Fries" />
-          <p>French Fries $2.99</p>
-        </div>
-      </button>
-
-      <button onClick={() => setCount((count) => count + 3.99)} class="menuItem">
-        <div>
-          <img src="public/IceCream.png" className="IceCream" alt="IceCream" />
-          <p>Ice Cream $3.99</p>
-        </div>
-      </button>
-
-    </div>
-
-    <div className='paymentInfo'>
-
-
-    <p className='orderInfo'>Order Info:</p>
-      <ul>
-        <li>MENU ITEM HERE</li>
-        <li>MENU ITEM HERE</li>
-        <li>MENU ITEM HERE</li>
-        <li>MENU ITEM HERE</li>
-        <li>MENU ITEM HERE</li>
-        <li>MENU ITEM HERE</li>
-      </ul>
-
-      <label for="CCInfo">Credit Card Number:</label>
-      <input type="text" id="CCInfo"></input>
-
-      <label for="CCV">CCV:</label>
-      <input type="text" id="CCV"></input>
-
-      <label for="CCDate">Exp. Date:</label>
-      <input type="text" id="CCDate"></input>
-
-      <label for="Address">Address:</label>
-      <input type="text" id="Address"></input>
-
-      <label for="Tip">Tip:</label>
-      <input type="text" id="Tip"></input>
-
-      <p><b>Total Price: ${count.toFixed(2)}</b></p>
-
-      <input type="button" className="orderButton" value="Place Order"></input>
-
-    </div>
-
-    <img src="public/ER.PNG" className="ER"></img>
-
-  </div>
+        <img src="public/ER.PNG" className="ER" alt="ER Diagram" />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
