90c4b2f75f62a50000b156c15c4ebe2b43d93b6b
Alejandro Martinez <Malejandrog2000@gmail.com>
2024-11-06 19:48:48 -0600

    Buttons now add price to the total

--- 3380HW4/src/App.jsx
+++ 3380HW4/src/App.jsx
@@ -16,84 +16,84 @@ function App() {
   <div className="mainContainer">
     <div class="menuArea">
 
-      <button class="menuItem">
+      <button onClick={() => setCount((count) => count + 8.99)} class="menuItem">
         <div>
           <img src="public/Burger1.png" className="Burger1" alt="Burger1" />
           <p>The Databurger $8.99</p>
         </div>
       </button>
       
-      <button class="menuItem">
+      <button onClick={() => setCount((count) => count + 9.99)} class="menuItem">
         <div>
           <img src="public/Burger2.png" className="Burger2" alt="Burger2" />
           <p>The Double Smash Patty $9.99</p>
         </div>
       </button>
 
-      <button class="menuItem">
+      <button onClick={() => setCount((count) => count + 7.99)} class="menuItem">
         <div>
           <img src="public/ChickenSandwich.png" className="ChickenSandwich" alt="ChickenSandwich" />
           <p>The Chicken Sandwich $7.99</p>
         </div>
       </button>
 
-      <button class="menuItem">
+      <button onClick={() => setCount((count) => count + 6.99)} class="menuItem">
         <div>
           <img src="public/ChickenTender.png" className="ChickenTender" alt="ChickenTender" />
           <p>Chicken Tenders $6.99</p>
         </div>
       </button>
 
-      <button class="menuItem">
+      <button onClick={() => setCount((count) => count + 12.99)} class="menuItem">
         <div>
           <img src="public/b1Meal.png" className="b1Meal" alt="b1Meal" />
           <p>Databurger Meal $12.99</p>
         </div>
       </button>
 
-      <button class="menuItem">
+      <button onClick={() => setCount((count) => count + 13.99)} class="menuItem">
         <div>
           <img src="public/b2Meal.png" className="b2Meal" alt="b2Meal" />
           <p>Double Patty Meal $13.99</p>
         </div>
       </button>
 
-      <button class="menuItem">
+      <button onClick={() => setCount((count) => count + 11.99)} class="menuItem">
         <div>
           <img src="public/CSMeal.png" className="CSMeal" alt="CSMeal" />
           <p>Chicken Sandwhich Meal $11.99</p>
         </div>
       </button>
 
-      <button class="menuItem">
+      <button onClick={() => setCount((count) => count + 10.99)} class="menuItem">
         <div>
           <img src="public/CTMeal.png" className="CTMeal" alt="CTMeal" />
           <p>Chicken Tender Meal $10.99</p>
         </div>
       </button>
 
-      <button class="menuItem">
+      <button onClick={() => setCount((count) => count + 6.99)} class="menuItem">
         <div>
           <img src="public/Salad.png" className="Salad" alt="Salad" />
           <p>Chicken Salad $6.99</p>
         </div>
       </button>
 
-      <button class="menuItem">
+      <button onClick={() => setCount((count) => count + 1.99)} class="menuItem">
         <div>
           <img src="public/Drink.png" className="Drink" alt="Drink" />
           <p>Fountain Drink $1.99</p>
         </div>
       </button>
 
-      <button class="menuItem">
+      <button onClick={() => setCount((count) => count + 2.99)} class="menuItem">
         <div>
           <img src="public/Fries.png" className="Fries" alt="Fries" />
           <p>French Fries $2.99</p>
         </div>
       </button>
 
-      <button class="menuItem">
+      <button onClick={() => setCount((count) => count + 3.99)} class="menuItem">
         <div>
           <img src="public/IceCream.png" className="IceCream" alt="IceCream" />
           <p>Ice Cream $3.99</p>
@@ -130,7 +130,7 @@ function App() {
       <label for="Tip">Tip:</label>
       <input type="text" id="Tip"></input>
 
-      <p><b>Price: $PLACEHOLDER</b></p>
+      <p><b>Price: {count}</b></p>
 
       <input type="button" className="orderButton" value="Place Order"></input>
 
